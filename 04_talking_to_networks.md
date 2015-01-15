4f6cbc9940dea119062f1a34cb62409ab6e143b8
DanielZhangQingLong <shangrenzhidao@126.com>
2015-01-16 07:47:26 +0800

    mv files

--- 04_talking_to_networks.md
+++
@@ -1,9 +0,0 @@
-## Talking to Networks
-
-```ruby
-require 'open-uri'
-
-open('http://www.baidu.com') do |f|
-  puts f.read.to_s
-end
-```
--- 05_parsing_html.rb
+++
@@ -1,7 +0,0 @@
-require 'open-uri'
-
-page = open('http://www.baidu.com').read
-
-if page =~ %r{<title>(.*?)</title>}m
-  puts "Title is #{$1.inspect}"
-end
